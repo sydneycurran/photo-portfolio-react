0e1c36a3e04a8953786efeb243db939080bd432c
Sydney Curran <sydneycurran@gmail.com>
2026-09-01 00:13:14 -0400

    Minor formatting updates

--- src/pages/Pets.jsx
+++ src/pages/Pets.jsx
@@ -57,11 +57,7 @@ const Pets = () => {
                     <hr/>
                     <h2 className="subhead">Doggos Of Chicago</h2>
                     <p className="page-text">
-                        "Doggos of Chicago" began as a personal project in April of 2020 because 
-                        while I couldn't go within 6 feet of other people to pet their dogs, I 
-                        could still take pictures of the dogs I saw in the park. Even though I'm 
-                        now free to pet the dogs I see in the park, I still enjoy going out and 
-                        taking pictures of the dogs around me. See more Doggos of Chicago on
+                        See more Doggos of Chicago on
                         the <InlineNavLink className="inline-link" to="/doggos-of-chicago">
                             Doggos of Chicago page
                             </InlineNavLink>.
